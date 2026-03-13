1da9de347dbb753f9cc621cab2f1f5676bf99d1c
Javohir Gulyamov <gulamovzavohir04@gmail.com>
2026-03-14 01:28:43 +0400

    fix superuser creation

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-set -e
 
-python manage.py migrate --noinput
+echo "Apply database migrations"
+python manage.py migrate
 
-exec gunicorn config.wsgi:application --bind 0.0.0.0:${PORT:-8000}
+echo "Create superuser if not exists"
+python manage.py createsuperuser --noinput || true
+
+echo "Collect static files"
+python manage.py collectstatic --noinput
+
+echo "Start server"
+gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
