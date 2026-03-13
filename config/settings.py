390b1538b022a37f852dd664a12befd068884bc9
Javohir Gulyamov <gulamovzavohir04@gmail.com>
2026-03-13 16:58:01 +0400

    fix railway database

--- config/settings.py
+++ config/settings.py
@@ -122,8 +122,8 @@ import dj_database_url
 import os
 
 DATABASES = {
-    "default": dj_database_url.parse(
-        os.environ.get("DATABASE_URL")
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
     )
 }
 # Password validation
